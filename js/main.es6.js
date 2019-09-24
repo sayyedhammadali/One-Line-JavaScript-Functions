44ecf866977e3dd4a380dab4a7ff16d70b85cb1f
sayyed hammad ali <hammad_ali447@yahoo.com>
2019-09-24 18:15:08 +0200

    step

--- js/main.es6.js
+++ js/main.es6.js
@@ -1,5 +1,7 @@
 (function($) {
 
+
+
   // Algo 1: Armchair Algorithms 0.0.1
   // Given an array of positive number inputs, design an algorithm that returns
   // an array of the same length where the values in the new array are the
@@ -8,9 +10,12 @@
   // eg. [3, 5, 9, 2] should return [90, 54, 30, 135]
   //
   // source: https://codeburst.io/armchair-algorithms-0-0-1-97b49a9060ab
+
   function armchair1() {
     /* solution here... */
   }
+
+
 
   // Algo 2: Armchair Algorithms 0.0.2
   // Given an Array of positive integers, determine whether that Array has any duplicates.
@@ -22,6 +27,8 @@
   function armchair2() {
     /* solution here... */
   }
+
+
 
   // Algo 3: Two Sum
   // Given an array of integers, return indices of the two numbers such that they add up
@@ -44,5 +51,33 @@
     }
   }
   // console.log('->', twoSum([11, 15, 2, 7], 9));
+
+
+
+  // Algo 4: Longest Substring Without Repeating Characters
+  // Given a string, find the length of the longest substring without repeating characters.
+  //
+  // e.g.
+  // Input: "abcabcbb"
+  // Output: 3
+  // Explanation: The answer is "abc", with the length of 3.
+  //
+  // e.g.
+  // Input: "bbbbb"
+  // Output: 1
+  // Explanation: The answer is "b", with the length of 1.
+  //
+  // e.g.
+  // Input: "pwwkew"
+  // Output: 3
+  // Explanation: The answer is "wke", with the length of 3.
+  // Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+  //
+  // source: leetcode
+
+  const lengthOfLongestSubstring = function(str) {
+    console.log('lengthOfLongestSubstring...');
+  }
+  console.log('->', lengthOfLongestSubstring('abcabcbb'));
 
 })(jQuery);
